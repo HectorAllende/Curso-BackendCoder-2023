ea23aa19061fe012192cf34d1e72d393498c5284
HectorAllende <54869462+HectorAllende@users.noreply.github.com>
2023-07-15 13:58:43 -0300

    Agregando metodo update
    se agrega metodo de actualizar

--- Trabajo-final/src/controllers/ProductManager.js
+++ Trabajo-final/src/controllers/ProductManager.js
@@ -1,22 +1,26 @@
-import {promises as fs} from 'fs'
+import { promises as fs } from 'fs'
 import { nanoid } from 'nanoid'
 
 
 class ProductManager {
-    constructor(){
-        this.path ='./src/models/products.json'
+    constructor() {
+        this.path = './src/models/products.json'
     }
 
-    readProducts = async () =>{
+    readProducts = async () => {
         let products = await fs.readFile(this.path, 'utf8')
         return JSON.parse(products)
     }
 
-    writeProduts = async (product) =>{
+    writeProduts = async (product) => {
         await fs.writeFile(this.path, JSON.stringify(product))
     }
 
-    addProducts = async (product) =>{
+    exist = async (id) => {
+        let products = await this.readProducts()
+        return products.find(el => el.id === id)
+    }
+    addProducts = async (product) => {
         let productsOld = await this.readProducts()
         product.id = nanoid()
         let productAll = [...productsOld, product]
@@ -24,8 +28,35 @@ class ProductManager {
         return "Producto Agregado"
     }
 
-    getProducts = async () =>{
+    getProducts = async () => {
         return await this.readProducts()
+    }
+
+    getProductsById = async (id) => {
+        let productById = await this.exist(id)
+        if (!productById) return 'Producto no encontrado'
+        return productById
+    }
+
+    updateProducts = async (id, product) => {
+        let productById = await this.exist(id)
+        if (!productById) return 'Producto no encontrado'
+        await this.deleteProducts(id)
+        let productOld = await this.readProducts()
+        let products = [{...product, id : id}, ...productOld]
+        await this.writeProduts(products)
+        return "Producto actualizado"
+    }
+
+    deleteProducts = async (id) => {
+        let products = await this.readProducts()
+        let existProducts = products.some(el => el.id === id)
+        if (existProducts) {
+            let filterProducts = products.filter(el => el.id != id)
+            await this.writeProduts(filterProducts)
+            return "Producto eliminado"
+        }
+        return "Producto a eliminar no encontrado"
     }
 }
 
--- Trabajo-final/src/index.js
+++ Trabajo-final/src/index.js
@@ -7,18 +7,33 @@ const app = express()
 const PORT = 4000
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/products", async(req, res)=>{
-    res.send( await product.getProducts())
+app.get("/products", async (req, res) => {
+    res.send(await product.getProducts())
+})
+app.get("/products/:id", async (req, res) => {
+    const id = req.params.id
+    res.send(await product.getProductsById(id))
 })
 
-
-app.post("/products", async(req, res) =>{
-   let newProduct = req.body
+app.post("/products", async (req, res) => {
+    let newProduct = req.body
     res.send(await product.addProducts(newProduct))
 })
 
-app.listen(PORT, ()=>{
+app.put("/products/:id", async (req, res) => {
+    let id = req.params.id
+    let updateProduct = req.body
+    res.send(await product.updateProducts(id, updateProduct))
+})
+
+app.delete('/products/:id', async (req, res) => {
+    let id = req.params.id
+    res.send(await product.deleteProducts(id))
+
+})
+
+app.listen(PORT, () => {
     console.log(`Servidor Express Puerto ${PORT}`)
 })
