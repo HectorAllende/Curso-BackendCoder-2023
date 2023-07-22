4e09c9592a666236bbb50f0466379e10bc91a481
HectorAllende <54869462+HectorAllende@users.noreply.github.com>
2023-07-22 17:15:35 -0300

    agregando socket

--- Desafio-6/src/app.js
+++ Desafio-6/src/app.js
@@ -1,21 +1,37 @@
 import express from "express"
+import viewRouter from "./routes/view.router.js"
 import productRouter from "./routes/products.router.js"
 import cartRouter from "./routes/carts.router.js"
 import { __dirname } from "./utils.js"
+import handlebars from "express-handlebars"
 
-const app=express()
-const PORT=8080;
-console.log(__dirname)
+import {Server} from "socket.io"
+
+const app = express()
+const PORT = 8080;
+
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static(__dirname+"/public"))
-app.use("/api",productRouter)
-app.use("/api",cartRouter)
+app.use(express.static(__dirname + "/public"))
+
+app.engine('handlebars', handlebars.engine())
+app.set("view engine", "handlebars")
+app.set("views", __dirname + "/views")
+
+app.use("/api", productRouter)
+app.use("/api", cartRouter)
+app.use("/", viewRouter)
 
 
 
-app.listen(PORT,()=>{
-    console.log("server is working")
+const httpServer = app.listen(PORT, () => {
+    console.log("server is working in port 8080")
+})
+
+const socketServer = new Server(httpServer)
+
+socketServer.on("connection", (socket)=>{
+    console.log("cliente conectado con id", socket.id)
 })
